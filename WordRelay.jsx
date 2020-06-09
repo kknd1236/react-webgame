8ef085ceca80840c0cc97e7f024a68232d46506f
parkhaesun <hspark@makebot.ai>
2020-06-09 17:41:26 +0900

    끝말잇기 시작 및 웹팩 추가

---
+++ WordRelay.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+    render() {
+        return <div>끝말잇기</div>;
+    }
+}
+
+module.exports = WordRelay;
